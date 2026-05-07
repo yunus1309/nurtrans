c2e86066bbe2a22b6c3ce42d8750f19617269e2e
yunus1309 <152485082+yunus1309@users.noreply.github.com>
2026-05-07 16:09:15 +0200

    Change local service ports to avoid common conflicts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8088',
+  baseURL: 'http://localhost:18088',
   headers: {
     'Content-Type': 'application/json',
   },
